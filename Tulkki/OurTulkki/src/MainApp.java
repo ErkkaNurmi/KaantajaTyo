a2f56479edf0aa5105ad21981ad402c60b05630a
ErkkaNurmi <43371916+ErkkaNurmi@users.noreply.github.com>
2019-02-10 17:27:56 +0200

    Ei mitään

--- Tulkki/OurTulkki/src/MainApp.java
+++ Tulkki/OurTulkki/src/MainApp.java
@@ -4,25 +4,29 @@ import java.io.InputStream;
 
 import org.antlr.v4.runtime.*;
 import org.antlr.v4.runtime.tree.*;
- 
-public class MainApp 
-{
-    @SuppressWarnings("deprecation")
+import org.antlr.v4.runtime.tree.pattern.ParseTreeMatch;
+import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
+
+public class MainApp {
+	@SuppressWarnings("deprecation")
 	public static void main( String[] args )
     {
     	ANTLRInputStream antlris;
         try {
-        	InputStream inputStream
-        		= new FileInputStream("C:\\MyTemp\\KaTe\\kesktulk\\KaantajaTyo-12_12_2018-J-\\KaantajaTyo-12_12_2018-J-\\AntlrKoe\\ManuaalisenGeneroinninKoe\\arithmeticsTest.txt");
-			antlris = new ANTLRInputStream(inputStream);
-		
-        OurLanguageLexer lexer = new OurLanguageLexer(antlris);
-        CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
-        OurLanguageParser parser = new OurLanguageParser(commonTokenStream);
+        	InputStream inputStream = new FileInputStream("F:\\Kurssit\\KaantajaRepo2\\arithmeticsTest.txt");
+        	antlris = new ANTLRInputStream(inputStream);
+        	
+        	OurLanguageLexer lexer = new OurLanguageLexer(antlris);
+        	CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+        	OurLanguageParser parser = new OurLanguageParser(commonTokenStream);
+        	ParseTree tree = parser.program();
+        	//ParseTreePattern ptp = parser.compileParseTreePattern("ID", OurLanguageParser.RULE_program);
+        	//ParseTreeMatch m = ptp.match(tree);
+        	System.out.println(tree.toString());
  
-        /*OurLanguageParser.FileContext fileContext = parser.file();                
-        MarkupVisitor visitor = new MarkupVisitor();                
-        visitor.visit(fileContext);*/
+        	/*OurLanguageParser.FileContext fileContext = parser.file();                
+        	MarkupVisitor visitor = new MarkupVisitor();                
+        	visitor.visit(fileContext);*/
         } catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
